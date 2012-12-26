cbf539309689202c8f1cd03078a52a8486cc70c6
David Peter <david@1000memories.com>
2012-12-26 10:55:26 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laypaper::Application.config.secret_token = '88c04f9f9e32f2b466f70921fcada5f3862ab8f006832163385eafb2752a9b1f57608c9a84ba7c7b80bb0cbc13bc28b8eb11f544b0c1449bac31ab5ccc469f4c'
